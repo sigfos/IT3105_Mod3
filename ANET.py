2ecda28300cdc9efa0bd6c9530198354f118dde6
Øyvor Salvesen Haldorsen <oyvorsh@dhcp-10-24-4-145.wlan.ntnu.no>
2018-11-06 15:23:11 +0100

    Make ANET class

--- ANET.py
+++ ANET.py
@@ -1,8 +1,46 @@
 from keras.models import Sequential, model_from_json
 from keras.layers import Dense
-from keras.optimizers import Adam
 import numpy as np
 import random
+
+
+class Anet:
+
+    def __init__(self, dims=[9, 5, 5, 9], input_act='relu', output_act='softmax', init='uniform',
+                 epochs=1, batch_size=2, verbose=True, loss='mse', optimizer="adam"):
+        self.dims = dims
+        self.input_act = input_act
+        self.output_act = output_act
+        self.init = init
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.verbose = verbose
+        self.loss = loss
+        self.optimizer = optimizer
+        self.model = None
+
+    def create_layers(self, dims, input_act, output_act):
+        layers = list()
+        layers.append(Dense(dims[1], input_dim=dims[0], init=self.init, activation=input_act))
+        if len(dims) > 3:
+            for dim in dims[2:-1]:
+                layers.append(Dense(dim, init=self.init, activation=input_act))
+        layers.append(Dense(dims[-1], init=self.init, activation=output_act))
+        return layers
+
+    def create_anet(self):
+        layers = self.create_layers(self.dims, self.input_act, self.output_act)
+        model = Sequential()
+        for layer in layers:
+            model.add(layer)
+        self.model = model
+
+    def train(self, filename):
+        cases = get_data(filename)
+        x_train, y_train = random_minibatch(cases, self.batch_size)
+        # Define the optimization
+        self.model.compile(loss=self.loss, optimizer=self.optimizer, metrics=["accuracy"])
+        self.model.fit(x_train, y_train, epochs=self.epochs, batch_size=self.batch_size, verbose=self.verbose)
 
 
 def get_data(filename):
@@ -22,37 +60,21 @@ def random_minibatch(cases, batch_size):
     for i in range(batch_size):
         rand = random.randint(0, len(cases)-1)
         batch_cases.append(cases[rand])
-    X_train = [batch_cases[i][0] for i in range(len(batch_cases))]
+    x_train = [batch_cases[i][0] for i in range(len(batch_cases))]
     y_train = [batch_cases[i][1] for i in range(len(batch_cases))]
-    return np.array(X_train), np.array(y_train)
+    print("x", np.array(x_train), "y", np.array(y_train))
+    return np.array(x_train), np.array(y_train)
 
 
 def add_data(x, label, filename):
     string = "\n"
     for value in x:
         string += (str(value) + ",")
-    string += str(label)
+    string += ";"
+    for value in label:
+        string += (str(value) + ",")
     file_obj = open(filename, 'a')
     file_obj.write(string)
-
-
-def init_keras(layers):
-    # Model specification
-    model = Sequential()
-    for layer in layers:
-        model.add(layer)
-    return model
-
-
-def create_layers(dims, input_act, output_act):
-    layers = list()
-    layers.append(Dense(dims[1], input_dim=dims[0], activation=input_act))
-    if len(dims) > 2:
-        for dim in dims[2:-1]:
-            layers.append(Dense(dim, activation=input_act))
-    layers.append(Dense(dims[-1], activation=output_act))
-    layers.append(Dense(5))
-    return layers
 
 
 def save_model(model):
@@ -77,49 +99,22 @@ def load_model():
 
 # må formatere slik at board håndteres rett
 def check_valid_move(board, choice):
-    flat_board = flatten_board(board)
-    if flat_board[choice] == 0:
+    if board[0][choice] == 0:
         return True
     return False
 
 
-def flatten_board(board):
-    return np.array(board).flatten().tolist()
-
-
 # må formatere slik at board håndteres rett
-def get_expanded_index(board):
+def get_expanded_index(board, model):
     # data_input = flatten_board(board)
     data_input = board
-    anet = load_model()
-    predicted = anet.predict_classes(data_input)[0]
+    print("predict:", model.predict(data_input))
+    predicted = model.predict_classes(data_input)[0]
     if check_valid_move(data_input, predicted):
         return predicted
     else:
         while True:
             move = random.randint(0, len(board)-1)
             if check_valid_move(board, move):
+                print("Choosing randomly")
                 return move
-
-
-def create_anet(dim=[9, 5, 5, 2, 2, 9], input_act='relu', output_act='sigmoid'):
-    layers = create_layers(dim, input_act, output_act)
-    model = init_keras(layers)
-    save_model(model)
-
-
-def train(epochs=10, batch_size=1, verbose=True, loss='sparse_categorical_crossentropy', optimizer=Adam()):
-    model = load_model()
-    cases = get_data("RBUF.txt")
-    x_train, y_train = random_minibatch(cases, batch_size)
-    # Define the optimization
-    model.compile(loss=loss, optimizer=optimizer, metrics=["accuracy"])
-    print(y_train.shape)
-    model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=verbose)
-    save_model(model)
-
-
-create_anet()
-train()
-test = [0, 1, -1, 0, -1, 0, 0, 1, 0]
-print(test, "predicted", get_expanded_index(np.array([test])))
